e2f95f623a7def7dd343c4271af69cfae5a7fb16
liulei <liu6uu@163.com>
2021-07-12 10:06:11 +0800

    新加环境变量

--- env.zsh
+++ env.zsh
@@ -1,4 +1,4 @@
 export EDITOR=nvim
 export GOROOT=/usr/local/go
 export GOPATH=~/go
-"export PATH=/home/liul/Software/GoLand-2021.1.3/bin:$PATH
+# export PATH=/home/liul/Software/GoLand-2021.1.3/bin:$PATH
