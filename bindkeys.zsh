e2beabc88cfacb10f37ad4d6c7ef77b75037ab25
liulei <liu6uu@163.com>
2022-07-25 16:15:43 +0800

    添加上下翻页的快捷键

--- bindkeys.zsh
+++ bindkeys.zsh
@@ -1,2 +1,7 @@
+# showkey -a
+# https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html
 bindkey '^e' autosuggest-accept
 bindkey '^x' autosuggest-execute
+bindkey '^k' up-line-or-search
+bindkey '^j' down-line-or-search
+
